98f2537c7859037247d18e0c7a729882cd1aa30a
fostopern <86517609+fostopern@users.noreply.github.com>
2022-12-29 10:58:20 +0100

    second test comment

--- .scripts/bar.py
+++ .scripts/bar.py
@@ -2,4 +2,4 @@ from os import system; from time import sleep
 while True:
     system('''xsetroot -name "[ $(date +"Day:%d ][ Time:%I:%M ")][ Battery: $(cat /sys/class/power_supply/BAT0/capacity)% ][ WiFi:$(iwgetid wlan0 --raw) ][ Audio:$(awk -F"[][]" '/Left:/ { print $2 }' <(amixer sget Master))  ]"''')
     sleep(60)
-# first comment
+# test comment
